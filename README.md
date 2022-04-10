a639f430fdef7b7dfaa112662dcfcc5292970e81
MehulRawal <mehulraval101@gmail.com>
2022-04-10 12:03:42 +0530

    Second Commit

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # cetecMehulTask
+
+#For testing please change the username and password for connection of mysql database in the code
+
+Task 1:
+make a REST endpoint (GET)
+/person/{person_id}/info
+GET /person/1/info  returns:
+{
+"name": "mike",
+"phone_number": "444-444-4444",
+"city" : "Austin",
+"state" : "TX",
+"street1": "213 South 1st St",
+"street2": "", 
+"zip_code": "78704",
+}
+
+Task 2:
+make a REST endpoint (POST)  / Create
+/person/create
+POST /person/create  accepts body: 
+{
+"name": "YOURNAME",
+"phone_number": "123-456-7890",
+"city" : "Sacramento",
+"state" : "CA",
+"street1": "112 Main St",
+"street2": "Apt 12", 
+"zip_code": "12345",
+}
+returns 200
+
+
+Person
+create table person(id int not null key auto_increment,  name varchar(255), age int);
+insert into person(id, name, age) values (1, "mike", 31), (2, "John", 20), (3, "Joseph", 20);
+You can assume the following data in person:
+Name, age, id
+mike , 31, 1
+John, 45, 2
+Joseph, 20, 3
+
+
+phone
+create table phone(id int not null key auto_increment,  number varchar(255), person_id int);
+insert into phone(id, person_id, number) values (1,1, "444-444-4444"), (8,2, "123-444-7777"), (3,3, "445-222-1234");
+You can assume the following data in phone:
+person_id, id, number
+1,1, 444-444-4444
+2,8, 123-444-7777
+3,3, 445-222-1234
+
+
+address
+create table address(id int not null key auto_increment,  city varchar(255), state varchar(255), street1 varchar(255), street2 varchar(255), zip_code varchar(255));
+insert into address(id ,  city , state , street1 , street2 , zip_code ) values (1,"Eugene", "OR", "111 Main St", "", "98765"), (2, "Sacramento", "CA", "432 First St", "Apt 1", "22221"), (3, "Austin", "TX", "213 South 1st St", "", "78704");
+You can assume the following data in address:
+id, city, state, street1, street2, zip_code
+1,Eugene, OR, "111 Main St", "", 98765
+2, Sacramento, CA, "432 First St", "Apt 1", 22221
+3, Austin, TX, "213 South 1st St", "", 78704
---
+++ task/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"net/http"
+	"encoding/json"
+	"io"
+	"bytes"
+)
+
+var db *sql.DB
+
+type Response struct {
+	Name    string `json:"name"`
+	Number  string `json:"phone_number"`
+	City    string `json:"city"`
+	State   string `json:"state"`
+	Street1 string `json:"street1"`
+	Street2 string `json:"street2"`
+	Zip     string `json:"zip_code"`
+}
+
+func main() {
+	fmt.Println("Go MySQL Tutorial")
+
+	// Open up our database connection.
+	// I've set up a database on my local machine using phpmyadmin.
+	// The database is called testDb
+	var err error
+	db, err = sql.Open("mysql", "username:password@/cetec")
+	fmt.Printf("Done %T", db)
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		fmt.Println(err)
+		panic(err.Error())
+	}
+
+	// defer the close till after the main function has finished
+	// executing
+
+	defer db.Close()
+	//gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	router.GET("/person/:Id/info", getPersonById)
+	router.POST("/person/create", createPerson)
+
+	router.Run("localhost:8080")
+
+}
+
+func getPersonById(c *gin.Context) {
+	id := c.Param("id")
+
+	results := db.QueryRow("SELECT name for person where Id= ?", id)
+	Resp := Response{}
+	err := results.Scan(&Resp.Name)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "person not found"}) // proper error handling instead of panic in your app
+	} else {
+
+		results = db.QueryRow("SELECT number for phone where person_id= ?", id)
+		err := results.Scan(&Resp.Number)
+		if err != nil {
+			c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Number not found"})
+		} else {
+			results = db.QueryRow("Select city,state,street1,street2,zip_code from address where id = ?", id)
+			err := results.Scan(&Resp.City, &Resp.State, &Resp.Street1, &Resp.Street2, &Resp.Zip)
+			if err != nil {
+				c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Address not found"})
+			} else {
+				c.IndentedJSON(http.StatusOK, Resp)
+			}
+
+		}
+	}
+}
+
+func createPerson(c *gin.Context) {
+
+	var requestBody Response
+	bytes := StreamToByte(c.Request.Body)
+   if err := json.Unmarshal(bytes,&requestBody); err != nil {
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": err.Error()})
+   }else{
+
+   }
+
+  fmt.Println(requestBody)
+}
+
+func StreamToByte(stream io.Reader) []byte {
+	buf := new(bytes.Buffer)
+	  buf.ReadFrom(stream)
+	  return buf.Bytes()
+  }
+  
